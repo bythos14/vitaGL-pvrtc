e47dad28ae7b4ece7728d8af7db528f9c8805cd3
bythos14 <richiehyacinth@gmail.com>
2020-08-13 10:16:09 -0400

    Use VBOs for vertex data.

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 // Drawing a fullscreen image on screen with glBegin/glEnd
 
 #include <vitaGL.h>
-#include <vita2d.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-GLenum texture_format = GL_COMPRESSED_RGBA_PVRTC_4BPPV1_IMG;
 GLuint texture = 0;
+GLuint buffers[2] = {0, 0};
 
 typedef struct PVRTextureHeader {
 	uint32_t version; // Should be 0x50565203
@@ -34,7 +33,7 @@ typedef struct PVRTextureHeader {
 	uint32_t metadataSize;
 } PVRTextureHeader;
 
-void* loadTexture(uint16_t* width, uint16_t* height)
+void* loadTexture(uint16_t* width, uint16_t* height, uint32_t* imagesize)
 {
 	FILE *fd = fopen("app0:texture.pvr", "rb");
 	fseek(fd, 0, SEEK_END);
@@ -50,19 +49,34 @@ void* loadTexture(uint16_t* width, uint16_t* height)
 	
 	void *textureData = buf + (sizeof(PVRTextureHeader) + header->metadataSize);
 	size_t tex_size = size - (sizeof(PVRTextureHeader) + header->metadataSize);
+	*imagesize = tex_size;
 	void* data = (unsigned char *)malloc(tex_size);
 	memcpy(data, textureData, tex_size);
 	free(buf);
 	return data;
 }
 
-int main(){
-	
+float vertices[] = 
+{
+	0.0f, 0.0f, 0.0f,
+	544.0f, 0.0f, 0.0f,
+	544.0f, 544.0f, 0.0f,
+	0.0f, 544.0f, 0.0f,
+	0.0f, 1.0f,
+	1.0f, 1.0f,
+	1.0f, 0.0f,
+	0.0f, 0.0f
+};
+
+uint16_t indices[] = 
+{
+	0, 1, 3,
+	1, 2, 3
+};
+
+int main() {
 	// Initializing graphics device
 	vglInit(0x800000);
-	
-	uint16_t w, h;
-	void* buffer = loadTexture(&w, &h);
 
 	glClearColor(0.50, 0, 0, 0);
 	glMatrixMode(GL_PROJECTION);
@@ -70,12 +84,23 @@ int main(){
 	glOrtho(0, 960, 544, 0, -1, 1);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
+	
+	// Generate buffers
+	glGenBuffers(2, buffers);
+	glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 20, vertices, GL_STATIC_DRAW);
 
-	// Initializing openGL texture
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[1]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint16_t) * 6, indices, GL_STATIC_DRAW);
+
+	// Loading texture from PVR file. 
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
 
-	glTexImage2D(GL_TEXTURE_2D, 0, texture_format, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, buffer);
+	uint16_t w, h;
+	uint32_t size;
+	void *buffer = loadTexture(&w, &h, &size);
+	glCompressedTexImage2D(GL_TEXTURE_2D, 0, GL_COMPRESSED_RGBA_PVRTC_4BPPV2_IMG, w, h, 0, size, buffer);
 	
 	glEnable(GL_TEXTURE_2D);
 	glEnable(GL_BLEND);
@@ -83,21 +108,21 @@ int main(){
 
 	for (;;){
 		vglStartRendering();
-		glClear(GL_COLOR_BUFFER_BIT);
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		glBindTexture(GL_TEXTURE_2D, texture);
-		glBegin(GL_QUADS);
-		
-		// Note: BMP images are vertically flipped
-		glTexCoord2i(0, 1);
-		glVertex3f(0, 0, 0);
-		glTexCoord2i(1, 1);
-		glVertex3f(544, 0, 0);
-		glTexCoord2i(1, 0);
-		glVertex3f(544, 544, 0);
-		glTexCoord2i(0, 0);
-		glVertex3f(0, 544, 0);
-		
-		glEnd();
+		glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers[1]);
+
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		glVertexPointer(3, GL_FLOAT, 0, 0);
+		glTexCoordPointer(2, GL_FLOAT, 0, 12 * sizeof(float));
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+
 		vglStopRendering();
 		glLoadIdentity();
 	}
